be8573c8e2fa3097bf761b55a8673e12966ed6db
KumudBhatt <kumud.bhatt444@gmail.com>
2024-11-19 02:42:28 +0530

    quick fix

--- main/frontend/src/axiosInstance.jsx
+++ main/frontend/src/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://13.53.152.19:3000/api/v1',
+  baseURL: 'https://13.53.152.19:3000/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
