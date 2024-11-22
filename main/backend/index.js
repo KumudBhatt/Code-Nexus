bae016cbc56c7d651401df9f6cb9fbdac41cc46b
KumudBhatt <kumud.bhatt444@gmail.com>
2024-11-23 00:44:39 +0530

    Update index.js

--- main/backend/index.js
+++ main/backend/index.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const http = require("http");
+const https = require("https");
 const { Server } = require("socket.io");
 const rootRouter = require("./routes/index.js");
 const cors = require("cors");
 const PORT = 3000;
 
 const app = express();
-const server = http.createServer(app);
+const server = https.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://codenexuslive.tech",
     methods: ["GET", "POST"],
   },
 });
